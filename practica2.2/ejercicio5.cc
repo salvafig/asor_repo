2b9289a905a7ea49f48561427da0b24b4dda2a98
salvafig <93118079+salvafig@users.noreply.github.com>
2021-11-12 12:37:56 +0100

    ejercicio5

--- practica2.2/ejercicio5.cc
+++ practica2.2/ejercicio5.cc
@@ -1,1 +1,11 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+int main(){
+  const char path[]="/pruebaEjercicio5.txt";
+  int archivo = open(path, O_CREAT, 645);
+  
+  return 1;
+}
