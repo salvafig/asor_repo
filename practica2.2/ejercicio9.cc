6f569348b59b6fd3b6d1e9fb93ac28bd8c01b56d
salvafig <93118079+salvafig@users.noreply.github.com>
2021-11-12 17:53:34 +0100

    ejercicio9

---
+++ practica2.2/ejercicio9.cc
@@ -0,0 +1,21 @@
+#include <sys/stat.h>
+#include <sys/types.h>
+#includes <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/sysmacros.h>
+#include <time.h>
+
+int main(int argc, char **argv){
+  struct stat informacion;
+  int info_stat = stat(argv[1], &informacion);
+  
+  printf("Major: %li\n", (long) major(informacion.st_dev));
+  printf("Minor: %li\n", (long) minor(informacion.st_dev));
+  printf("Inodo: %li\n", (long) informacion.st_ino);
+  printf("Tipo de archivo: %i\n", informacion.st_mode);
+  printf("Último Acceso: %s\n", ctime(informacion.st_atime));
+
+
+  return 0;
+}
