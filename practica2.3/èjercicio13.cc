a391957eb7d98557bb33ce3aca4d4aaa8c2c4607
salvafig <93118079+salvafig@users.noreply.github.com>
2021-11-25 21:13:50 +0100

    Ejercicio13

---
+++ practica2.3/èjercicio13.cc
@@ -0,0 +1,47 @@
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+bool delete_process = true;
+bool usr_interrupted = false;
+
+void handler(int senial){
+	delete_process = false;
+	usr_interrupted = true;
+
+	return;
+}
+
+
+/* ejercicio13 seconds */
+int main(int argc, char **argv){
+	struct sigaction sa;
+	sigset_t mask, old_mask;
+
+	sa.sa_handler = handler;
+	sa.sa_flags = SA_RESTART;
+	sigaction(SIGUSR1, &sa, NULL);
+
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGUSR1);
+
+	sigprocmask(SIG_BLOCK, &mask, &old_mask);
+	int i = 0;
+	sleep(atoi(argv[1]));
+	while(!usr_interrupted){
+		printf("HOLA\n");
+		sleep(1);
+		sigsuspend(&old_mask);
+		i++;
+	}
+	sigprocmask(SIG_UNBLOCK, &mask, NULL);
+
+	//sleep(argv[1])
+
+	if(delete_process){
+		unlink(argv[0]);
+	}
+	
+	return 0;
+}
