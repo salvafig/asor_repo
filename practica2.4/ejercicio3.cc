b9ff89f42d642b56dff1d7b3d05079d618998fff
salvafig <93118079+salvafig@users.noreply.github.com>
2021-11-30 23:59:19 +0100

    Create ejercicio3.cc

---
+++ practica2.4/ejercicio3.cc
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main(int argc, char **argv){
+	const char *pathname = "pipe_name";
+	
+	if(mkfifo(pathname, S_IRWXU | S_IRWXG) != -1){
+		printf("Pipe opened.\n");
+	}else{
+		printf("Could not open pipe.\n");
+		fprintf(stderr, "%s\n", strerror(errno));
+		return 1;
+	}
+
+
+	return 0;
+}
