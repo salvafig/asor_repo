0ea4b846e659a4b6725ff392ab8e1e202d08fcb1
salvafig <93118079+salvafig@users.noreply.github.com>
2021-12-21 18:13:51 +0100

    Update ejercicio2.cc
    ejercicio2.cc finished

--- practica2.5/ejercicio2.cc
+++ practica2.5/ejercicio2.cc
@@ -4,9 +4,10 @@
 #include <string.h>
 #include <iostream>
 #include <errno.h>
+#include <time.h>
 
 
-/* ./ejercicio1 hostname */
+/* ./ejercicio2 hostname portname */
 int main(int argc, char **argv){
 	struct addrinfo hints;
 	struct addrinfo *res;
@@ -17,13 +18,10 @@ int main(int argc, char **argv){
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 	
-	int rc = getaddrinfo(argv[1], NULL, &hints, &res);
+	int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
 	
 	if(rc != 0){
 		fprintf(stderr, "getaddrinfo: %s\n", strerror(errno));
-		/*std::cout << "getaddrinfo: "
-			<< strerror(rc)
-			<< std::endl;*/
 		return 1;
 	}
 	
@@ -34,30 +32,53 @@ int main(int argc, char **argv){
 		char buffer[80];
 		struct sockaddr_storage client;
 		socklent_t client_len;
-		
-		int bytes = recvfrom();
-	
-	}																																																												
-	
-	/*
-	struct addrinfo *i;
-	
-	for(i = res; i != NULL; i = i->ai_next){
 		char host[NI_MAXHOST];
 		char serv[NI_MAXSERV];
 		
-		int rc = getnameinfo(i->ai_addr, i->ai_addrlen, host, NI_MAXHOST,
+		int bytes = recvfrom(sd, buffer, 79, 0, (struct sockaddr*) client, &client_len);
+		
+		if(bytes == -1){
+			fprintf(stderr, "getnameinfo: %s\n", strerror(errno));
+			return 1;		
+		}
+		
+		buffer[bytes] = '\0';
+		
+		int rc = getnameinfo((struct sockaddr*) &client, client_len, host, NI_MAXHOST,
 					serv, NI_MAXSERV, NI_NUMERICHOST);
 		
 		if(rc != 0){
 			fprintf(stderr, "getnameinfo: %s\n", strerror(errno));
 			return 1;
 		}
-					
-		std::cout << host << " " << i->ai_family << " " << i->ai_socktype << std::endl;
 		
-	}
-	*/
+		std::cout << bytes << "bytes de "<< host << serv  << std::endl;
+		printf("%\n", bytes, host, serv);
+		
+		time_t now;
+		struct tm *timeinfo;
+		
+		if(strcmp(buffer, 't') == 0){
+			char buffer_time[9];
+			time(&now);
+			timeinfo = localtime(&now);
+			sprintf(buffer_time, "%d:%d:%d", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+			sendto(sd, buffer_time, strlen(buffer_time), 0, (struct sockaddr*) &client, client_len);
+		}else if(strcmp(buffer, 'd') == 0){
+			char buffer_date[11];
+			time(&now);
+			timeinfo = localtime(&now);
+			sprintf(buffer_date, "%d-%d-%d", timeinfo->tm_year + 1900, timeinfo->tm_mon + 1, timeinfo->tm_mday);
+			sendto(sd, buffer_date, strlen(buffer_date), 0, (struct sockaddr*) &client, client_len);
+		}else if(strcmp(buffer, 'q') == 0){
+			std::cout << "Saliendo..."<< std::endl;
+			break;
+		}else{
+			std::cout << "Comando "<< buffer << " no soportado"  << std::endl;
+		}
+		
+	}																																																												
+
 	freeaddrinfo(res);
 	
 
