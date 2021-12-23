c79bf83a9d9e69db2dab91ec3313ababde4a83ea
salvafig <93118079+salvafig@users.noreply.github.com>
2021-12-23 13:16:37 +0100

    Create ejercicio5.cc
    ¿Es necesario cerrar el fichero que hace referencia al socket en la línea 93?
    UDP socket programming - fork server on recev: https://stackoverflow.com/questions/7858877/udp-socket-programming-fork-server-on-recev
    UDP Socket Programming - James Madison University: https://users.cs.jmu.edu/bernstdh/web/common/lectures/summary_unix_udp.php

---
+++ practica2.5/ejercicio5.cc
@@ -0,0 +1,105 @@
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <string.h>
+#include <iostream>
+#include <errno.h>
+#include <time.h>
+#include <unistd.h>
+
+/* ./ejercicio5 hostname portname */
+int main(int argc, char **argv){
+	struct addrinfo hints;
+	struct addrinfo *res;
+	
+	memset(&hints, 0, sizeof(struct addrinfo));
+	
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	
+	int rc = getaddrinfo(argv[1], argv[2], &hints, &res);
+	
+	if(rc != 0){
+		fprintf(stderr, "getaddrinfo: %s\n", strerror(errno));
+		return 1;
+	}
+	
+	int sd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	bind(sd, res->ai_addr, res->ai_addrlen);
+	
+	while(1){
+		//char buffer[80];
+		struct sockaddr_storage client;
+		socklent_t client_len = sizeof(struct sockaddr_storage);
+		char host[NI_MAXHOST];
+		char serv[NI_MAXSERV];
+		
+		int bytes = recvfrom(sd, buffer, 79, 0, (struct sockaddr*) client, &client_len);
+		
+		if(bytes == -1){
+			fprintf(stderr, "getnameinfo: %s\n", strerror(errno));
+			return 1;		
+		}else if(bytes > 0){
+				// a connection has been established
+			buffer[bytes] = '\0';
+			
+			int rc = getnameinfo((struct sockaddr*) &client, client_len, host, NI_MAXHOST,
+					serv, NI_MAXSERV, NI_NUMERICHOST);
+		
+			if(rc != 0){
+				fprintf(stderr, "getnameinfo: %s\n", strerror(errno));
+				return 1;
+			}
+			
+			std::cout << bytes << "bytes de "<< host << serv  << std::endl;
+			
+			pid_t pid = fork();
+			switch(pid){
+				case -1:
+					fprintf(stderr, "fork: %s\n", strerror(errno));
+					exit(1);
+				case 0:
+						//Proceso hijo
+					time_t now;
+					struct tm *timeinfo;
+					
+					std::cout << "PID: "<< getpid() << std::endl;
+					
+					if(strcmp(buffer, 't') == 0){
+						char buffer_time[9];
+						time(&now);
+						timeinfo = localtime(&now);
+						sprintf(buffer_time, "%d:%d:%d", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
+						sendto(sd, buffer_time, strlen(buffer_time), 0, (struct sockaddr*) &client, client_len);
+					}else if(strcmp(buffer, 'd') == 0){
+						char buffer_date[11];
+						time(&now);
+						timeinfo = localtime(&now);
+						sprintf(buffer_date, "%d-%d-%d", timeinfo->tm_year + 1900, timeinfo->tm_mon + 1, timeinfo->tm_mday);
+						sendto(sd, buffer_date, strlen(buffer_date), 0, (struct sockaddr*) &client, client_len);
+					}else if(strcmp(buffer, 'q') == 0){
+						std::cout << "Saliendo..."<< std::endl;
+						break;
+					}else{
+						std::cout << "Comando "<< buffer << " no soportado"  << std::endl;
+					}
+					close(sd);
+				case 1:
+						//Proceso padre
+					break;
+			}
+			
+			close(sd); // La idea es cerrar el fichero que hace referencia al socket aquí, pero, ¿es necesario? 
+				
+		}
+
+		
+		
+	}																																																												
+
+	freeaddrinfo(res);
+	
+
+	return 0;
+}
