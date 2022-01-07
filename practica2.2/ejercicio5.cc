267c1c6bfae3f4344c9cf2278fa0b18e52a12307
salvafig <93118079+salvafig@users.noreply.github.com>
2022-01-07 16:01:27 +0100

    Update ejercicio5.cc for Exam

--- practica2.2/ejercicio5.cc
+++ practica2.2/ejercicio5.cc
@@ -1,11 +1,19 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <sys/stat.h>
 
-int main(){
-  const char path[]="/pruebaEjercicio5.txt";
-  int archivo = open(path, O_CREAT, 645);
-  
-  return 0;
+
+/* ./ejercicio5 */
+int main(int argc, char **argv){
+
+	//int fd = open("file_practica2.2_ejercicio5", O_CREAT, 0645);
+	int fd = open("file_practica2.2_ejercicio5", O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH | S_IXOTH);
+	if(fd == -1){
+		fprintf(stderr, "open: %s\n", strerror(errno));
+		return 1;
+	}
+
+	return 0;
 }
